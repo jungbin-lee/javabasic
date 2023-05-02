b215c142f515a66a434855cee5330adf7c636603
jungbin-lee <hihihi486@naver.com>
2023-05-02 10:22:40 +0900

    20230502 classanswer

--- src/main/java/String7.java
+++ src/main/java/String7.java
@@ -1,16 +1,32 @@
 import java.util.Scanner;
 
 public class String7 {
-    public String solution(String str){
-        String answer= "";
-        char[] s =str.toCharArray();
-        char lt= s[0];
-        char rt= s[s.length-1];
-        while (lt==rt){
-            if (lt==rt) lt++;
+    public String solution(String str)
+//    {
+//        String answer= "YES";
+//        str = str.toUpperCase();
+////        char[] s =str.toCharArray();
+////        char lt= s[0];
+////        char rt= s[s.length-1];
+////        while (lt==rt){
+////            if (lt==rt) lt++;
+////
+////        }
+//
+//        //짝홀 둘다 상관없다.
+//        int len = str.length();
+//        for (int i =0; i<len/2; i++){
+//            if (str.charAt(i)!=str.charAt(len-i-1))
+//                return "NO";
+//        }
+//        return answer;
+//    }
 
-        }
-        return null;
+    {
+        String answer= "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(tmp)) answer="YES";
+            return answer;
     }
 
     public static void main(String[] args){
